27cb08b2885753bb5ce0f59d3cbd3ab879dee6d4
DubzChemistry <139189196+DubzChemistry@users.noreply.github.com>
2025-02-04 00:44:18 -0500

    Add files via upload

---
+++ funcodingstuff.js
@@ -0,0 +1,18 @@
+const wordListFour = ["bevy", "baby", "womb", "bomb", "ably", "bawl", "blew", "blow", "bony", "bowl", "byte", "newb", "obey", "vibe", "babe", "balm", "beam", "bibe", "blab", "blob", "blub", "bulb", "lamb", "limb", "numb", "tomb", "able", "bail", "bait", "bale", "bane", "bate", "beal", "bean", "beat", "belt", "bent", "beta", "bile", "bite", "blue", "boat", "boil", "bolt", "bone", "bunt", "ebon", "tuba", "tube", "unbe"];
+const wordListFive = ["webby", "womby", "blowy", "bylaw", "balmy", "beamy", "bilby", "blimy", "embay", "embow", "lamby", "limby", "lobby", "maybe", "nobby", "nubby", "tabby", "tubby", "bimbo", "above", "banty", "belay", "blawn", "bliny", "blown", "bluey", "boney", "bowel", "butyl", "ebony", "elbow", "inbye", "nobly", "outby", "abbot", "album", "ambit", "amble", "babel", "bible", "bilbo", "biome", "blame", "blume", "imbue", "limbo", "umbel", "aboil", "about", "baton", "bento", "binal", "biont", "biota", "blain", "blate", "bleat", "blent", "bloat", "blunt", "built", "noble", "table", "tubae", "tubal"];
+const wordListSix = ["blowby", "wabbly", "byelaw", "viably", "blimey", "bowman", "bowmen", "byname", "nibbly", "nimbly", "numbly", "nibbly", "wabble", "wimble", "wobble", "wombat", "bumble", "beauty", "botany", "bounty", "bovine", "byline", "outbye", "viable", "bauble", "mobile", "nobble", "tumble", "albeit", "albino", "butane", "nebula", "obtain", "oblate", "unable", "unbelt", "unbolt"];
+const wordListSeven = ["movably", "beltway", "beamily", "buyable", "movable", "mutably", "nobbily", "bambino", "baloney", "bayonet", "buoyant", "notably", "ownable", "teabowl", "towable", "votable", "ambient", "bimetal", "bobtail", "lambent", "minable", "mutable", "outbeam", "bailout", "butanol", "notable", "tabouli", "tunable"];
+const wordListEight = ["moveably", "blowtube", "obviable", "bivalent", "tuneably", "unviable", "bailment", "nobelium", "ablution", "abutilon", "tailbone"];
+const wordListNine = ["unmovable", "bombinate", "mountable"];
+const wordListTen = ["obnubilate"];
+
+var wordCount = 0;
+
+function addLetV() {
+    var inputtedWord = document.getElementById("test");
+    inputtedWord.textContent += "V"
+}
+
+function submitWord() {
+    
+}
